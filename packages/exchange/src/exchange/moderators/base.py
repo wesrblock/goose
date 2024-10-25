5b027ca6af4a2f4d24e9e53ebd212dabbaf06517
Mic Neale <michael.neale@gmail.com>
2024-10-25 15:25:05 +1100

    small fixes

--- packages/exchange/src/exchange/moderators/base.py
+++ packages/exchange/src/exchange/moderators/base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from goose.notifier import Notifier
 
+
 class Moderator(ABC):
     def __init__(self, notifier: Notifier | None = None) -> None:
         self.notifier = notifier
