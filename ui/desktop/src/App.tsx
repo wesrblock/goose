557208b31e7651dff555ce00365c12d852600a01
Alex Hancock <alexhancock@block.xyz>
2024-11-12 16:28:46 -0500

    [app] Fix crasher in distributable builds by using HashRouter

--- ui/desktop/src/App.tsx
+++ ui/desktop/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Chat from './Chat'
 
