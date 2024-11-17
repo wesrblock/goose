434300c8ce3131f684aeada946c8c5e40bae7342
Jarrod Sibbison <jsibbison@squareup.com>
2024-11-17 11:34:26 +1100

    adds light and dark theme

--- crates/goose-cli/src/prompt.rs
+++ crates/goose-cli/src/prompt.rs
@@ -1,7 +1,6 @@
 use std::io::{self, Write};
 
 use anyhow::Result;
-use bat::PrettyPrinter;
 use cliclack::{input, spinner};
 use goose::providers::types::message::Message;
 
@@ -23,9 +22,15 @@ pub enum InputType {
     Exit,    // User wants to exit the session
 }
 
+pub enum Theme {
+    Light,
+    Dark,
+}
+
 pub struct CliclackPrompt {
     spinner: cliclack::ProgressBar,
     input_mode: InputMode,
+    theme: Theme,
 }
 
 enum InputMode {
@@ -34,9 +39,25 @@ enum InputMode {
 }
 impl CliclackPrompt {
     pub fn new() -> Self {
+        // // Load highlighting assets
+        // let assets = HighlightingAssets::from_binary();
+
+        // // Fetch and list all available themes
+        // let themes = assets.themes();
+        // for theme_name in themes {
+        //     println!("{}", theme_name);
+        // }
+
+        // // List all available syntaxes (languages)
+        // let syntax_set = assets.get_syntaxes().unwrap();
+        // for syntax in syntax_set {
+        //     println!("{}", syntax.name);
+        // }
+
         CliclackPrompt {
             spinner: spinner(),
             input_mode: InputMode::Multiline,
+            theme: Theme::Dark,
         }
     }
 }
@@ -44,9 +65,15 @@ impl CliclackPrompt {
 impl Prompt for CliclackPrompt {
     fn render(&mut self, message: Box<Message>) {
         let content = message.summary();
-        PrettyPrinter::new()
+
+        let theme = match self.theme {
+            Theme::Light => "GitHub",
+            Theme::Dark => "zenburn",
+        };
+        bat::PrettyPrinter::new()
             .input_from_bytes(content.as_bytes())
-            .language("markdown")
+            .theme(theme)
+            .language("Markdown")
             .print()
             .unwrap();
         io::stdout().flush().expect("Failed to flush stdout");
@@ -61,7 +88,7 @@ impl Prompt for CliclackPrompt {
     }
 
     fn get_input(&mut self) -> Result<Input> {
-        let mut input = input("Talk to Goose:L").placeholder("");
+        let mut input = input("Goose Chat: ( O)>         [Help: /?]").placeholder("");
         match self.input_mode {
             InputMode::Multiline => input = input.multiline(),
             InputMode::Singleline => (),
@@ -79,6 +106,26 @@ impl Prompt for CliclackPrompt {
             return self.get_input();
         } else if message_text.eq_ignore_ascii_case("/s") {
             self.input_mode = InputMode::Singleline;
+            return self.get_input();
+        } else if message_text.eq_ignore_ascii_case("/t") {
+            self.theme = match self.theme {
+                Theme::Light => {
+                    println!("Switching to Dark theme");
+                    Theme::Dark
+                }
+                Theme::Dark => {
+                    println!("Switching to Light theme");
+                    Theme::Light
+                }
+            };
+            return self.get_input();
+        } else if message_text.eq_ignore_ascii_case("/?") {
+            println!("Commands:");
+            println!("/exit - Exit the session");
+            println!("/m - Switch to multiline input mode");
+            println!("/s - Switch to singleline input mode");
+            println!("/t - Toggle Light/Dark theme");
+            println!("/? - Display this help message");
             return self.get_input();
         } else {
             return Ok(Input {
