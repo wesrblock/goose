9ffdbb64e7e861658ed1c7dd422b313bb074c866
Wendy Tang <wendytang@squareup.com>
2024-12-03 11:36:36 -0800

    rm process.env

--- ui/desktop/src/renderer.tsx
+++ ui/desktop/src/renderer.tsx
@@ -6,12 +6,16 @@ import App from './App'
 // Initialize Datadog RUM
 import { datadogRum } from '@datadog/browser-rum';
 
+const DATADOG_APPLICATION_ID = '139d4946-a1a5-4d5f-b017-2422e7774b9d';
+const DATADOG_CLIENT_TOKEN = 'puba9048a40434f456895695b2d552b9c5c';
+const DATADOG_ENV = 'dev'; // TODO: Change this to prod in the deployment pipeline
+
 datadogRum.init({
-    applicationId: process.env.DATADOG_APPLICATION_ID,
-    clientToken: process.env.DATADOG_CLIENT_TOKEN,
+    applicationId: DATADOG_APPLICATION_ID,
+    clientToken: DATADOG_CLIENT_TOKEN,
     site: 'datadoghq.com',
     service: 'goose',
-    env: 'prod',
+    env: DATADOG_ENV,
     sessionSampleRate: 100,
     sessionReplaySampleRate: 20,
     trackUserInteractions: true,
