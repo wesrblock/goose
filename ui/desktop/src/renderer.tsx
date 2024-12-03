839fa2c107fb497cb2dcb4caa2e9f617e91485b9
Wendy Tang <wendytang@squareup.com>
2024-12-03 11:16:47 -0800

    add loggins

--- ui/desktop/src/renderer.tsx
+++ ui/desktop/src/renderer.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
+// Initialize Datadog RUM
+import { datadogRum } from '@datadog/browser-rum';
+import dotenv from 'dotenv';
+
+datadogRum.init({
+    applicationId: process.env.DATADOG_APPLICATION_ID,
+    clientToken: process.env.DATADOG_CLIENT_TOKEN,
+    site: 'datadoghq.com',
+    service: 'goose',
+    env: 'prod',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
